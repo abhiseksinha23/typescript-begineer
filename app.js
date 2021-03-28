3cacb46d9dd5ad821f2006fdff5a09930e15fead
Abhisek Kumar <42487773+abhiseksinha23@users.noreply.github.com>
2021-03-28 23:19:26 +0530

    learning

--- app.js
+++ app.js
@@ -1,1 +1,7 @@
-console.log("hello There!!");
+function add(n1, n2) {
+    return n1 + n2;
+}
+var n1 = 2;
+var n2 = 3;
+var res = add(n1, n2);
+console.log(res);
--- app.ts
+++ app.ts
@@ -1,1 +1,10 @@
-console.log("hello There!!");
+function add(n1: number,n2: number){
+    return n1+n2;
+}
+
+const n1 = 2;
+
+const n2 = 3;
+
+const res = add(n1,n2);
+console.log(res);
